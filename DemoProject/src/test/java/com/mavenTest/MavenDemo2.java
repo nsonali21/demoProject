77ac732b6fdb574d5e7ae2f9d3f97a2cb78e2010
nsonali21 <71640450+nsonali21@users.noreply.github.com>
2023-07-30 18:02:05 +0530

    xyz

---
+++ DemoProject/src/test/java/com/mavenTest/MavenDemo2.java
@@ -0,0 +1,19 @@
+package com.mavenTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MavenDemo2 {
+@Test
+public void login() {
+	System.setProperty("webdriver.chrome.driver", "C:/TestData/chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("file:///C:/javabykiran-Selenium-Softwares/Offline%20Website/index.html");
+	driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+	driver.findElement(By.id("password")).sendKeys("123456");
+	
+	driver.findElement(By.xpath("//button")).click();
+}
+}
